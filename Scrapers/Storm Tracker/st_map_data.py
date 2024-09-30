99d8ff81ceedab4f99c3b0c95ad920add12c2299
gerard <12766134+scripting-drafts@users.noreply.github.com>
2024-09-30 13:38:44 +0200

    weather update revert

--- Scrapers/Storm Tracker/st_map_data.py
+++ Scrapers/Storm Tracker/st_map_data.py
@@ -3,31 +3,17 @@ from io import BytesIO
 from PIL import Image
 import folium
 import pandas as pd
-from pyproj import Transformer
 import base64
 
-t = Transformer.from_crs(4216, 4326)    # (4216, 4326)      default 3857
 eur_latitude, eur_longitude = 45.4, 16.17
 aa_latitude, aa_longitude = 44., 135.
 latitude, longitude = aa_latitude, aa_longitude
 
-# https://view.eumetsat.int/geoserver/ows?service=WMS&request=GetMap&version=1.3.0&layers=msg_fes:h60b&styles=&format=image/geotiff&crs=EPSG:4326&bbox=-77,-77,77,77&width=800&height=800
 df = pd.read_csv('coords_asia_img.csv', delimiter=';')  # 'Samples/bulkall.csv'
 
-tomtom_API_Key = 'XrZAUB7qYWfBWHgPfJi9msE3Hl7FlwsB'
-# https://tile.gbif.org/{srs}/{tileset}/{z}/{x}/{y}{format}{params}
-# https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}'
-# https://api.tomtom.com/map/1/wms/?key={Your_API_Key}&service=WMS&version=1.1.1&request=GetMap&bbox=1.355233,42.982261,24.980233,56.526017&srs=EPSG:4326&width=1305&height=748&layers=basic&styles=&format=image/png
 m = folium.Map(
     location=[latitude, longitude], # list(t.transform(latitude, longitude))
     zoom_start=5,
-    # control_scale=True,
-    # max_bounds=True,
-    # max_zoom=2,
-    # max_native_zoom=1,
-    # crs='EPSG4326',
-    # tiles='https://tile.gbif.org/4326/omt/0/1/0@4x.png?style=gbif-natural-en',
-    # tiles = f'http://localhost:8080/demo/wms/?srs=EPSG%3A4326&format=image%2Fpng&wms_layer=osm',
     attr="<a href=https://github.com/scripting-drafts/>scripting-drafts</a>"
 )
 
@@ -69,7 +55,6 @@ for row in range(0, len(df.index)):
 
     im = base64.b64decode(img_url.encode('utf_8_sig'))
     img = Image.open(BytesIO(im))
-    # img.show(img)
     buffer = img.resize((210,118))   # 210,118 - 158,89
     
     buffered = BytesIO()
@@ -77,11 +62,11 @@ for row in range(0, len(df.index)):
     img_url = base64.b64encode(buffered.getvalue()).decode('utf_8_sig')
 
     html = '<img src="data:image/png;base64,{}"><p></p><a href="{}" target="_blank" >{}</a>'.format
-    iframe = folium.IFrame(html(img_url,url,url_frontend), width=245japan, height=180)  # Original 420 236
+    iframe = folium.IFrame(html(img_url,url,url_frontend), width=245, height=180)
     popup = folium.Popup(iframe, max_width=270)
 
     folium.CircleMarker(
-        location=[lat, lon], #list(t.transform(lat, lon)),
+        location=[lat, lon],
         radius=4, # radius
         tooltip=url_frontend,
         fill=True,
